e29b7f28e3b927e5df6cdebd8302d70ac5765959
Валентина Кириленко <www.valentina@list.ru>
2023-08-14 23:29:45 +0300

    Modify UserModel, some tests

--- accounts/models.py
+++ accounts/models.py
@@ -12,7 +12,7 @@ class MyAccountManager(BaseUserManager):
         Create and save a User with the given email and password.
         """
         if not email:
-            raise ValueError('User must have an email address')      
+            raise ValueError('User must have an email address')
         email = self.normalize_email(email)
         user = self.model(email=email, **extra_fields)
         user.set_password(password)
@@ -21,11 +21,11 @@ class MyAccountManager(BaseUserManager):
 
     def create_superuser(self, email, password, **extra_fields):
         """
-        Create and save a SuperUser with the given email and password.
+        Creates and saves a Superuser with the given email and password.
         """
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
-        extra_fields.setdefault('is_active', True)
+        extra_fields.setdefault('is_admin', True)
         if extra_fields.get('is_staff') is not True:
             raise ValueError(_('Superuser must have is_staff=True.'))
         if extra_fields.get('is_superuser') is not True:
@@ -37,17 +37,17 @@ class Account(AbstractBaseUser):
     first_name = models.CharField(max_length=50)
     last_name = models.CharField(max_length=50)
     username = models.CharField(max_length=50, unique=True)
-    email = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=50, unique=True)
     date_join = models.DateTimeField(
         verbose_name=('Дата регистрации'),
         auto_now_add=True
     )
     last_login = models.DateTimeField(auto_now_add=True)
+    is_active = models.BooleanField(default=True)
     is_admin = models.BooleanField(default=False)
     is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=False)
-    is_superadmin = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
@@ -57,8 +57,20 @@ class Account(AbstractBaseUser):
     def __str__(self):
         return self.email
     
-    def has_perm(self, perm, obj=None):
-        return self.is_admin
+    def get_full_name(self):
+        """Returns full name."""
+        return f'{self.first_name} {self.last_name}'
+        # full_name = '%s %s' % (self.first_name, self.last_name)
+        # return full_name.strip()
+
+    def get_short_name(self):
+        """Returns short name."""
+        return self.first_name
     
-    def has_module_perm(self, add_label):
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        return True
+
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
         return True
--- accounts/tests.py
+++ accounts/tests.py
@@ -10,3 +10,16 @@ class UserManagersTests(TestCase):
             email='user@test.com',
             password='test'
         )
+        self.assertEqual(
+            user.email,
+            'user@test.com'
+        )
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_admin)
+
+    def test_create_user_no_arguments(self):
+        User = get_user_model()
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+
